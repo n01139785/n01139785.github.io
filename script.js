a8245a9225544304bb2ebbcc15aad5a46bb969a9
n01139785 <42619049+n01139785@users.noreply.github.com>
2019-10-25 22:22:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -152,11 +152,11 @@ dupeCheck();
     //location.assign("Results.html");
 
         document.getElementById("Part1").innerHTML = `<h2>Here Are Your Mission Matches!</h2>  
-<textarea id="Result1" readonly><</textarea> <img src="MMpics/MM_gaut2.jpg" alt="Logo" style="width:120px;height:120px;border:0;"> <br> <textarea id="Result2" readonly><</textarea> <img src="MMpics/MM_haiti4.jpg" alt="Logo" style="width:120px;height:120px;border:0;"><br> <textarea id="Result3" readonly><</textarea><img src="MMpics/MM_london.jpg" alt="Logo" style="width:120px;height:120px;border:0;"><br>
+<textarea id="Result1" readonly><</textarea>  <br> <textarea id="Result2" readonly><</textarea> <br> <textarea id="Result3" readonly><</textarea> <br>
 <p>Go check out the booths located under the welcome pavilion or the link below for more information.</p>
 <br> <a  href="https://app.managedmissions.com/OurTrips/4780" target="_blank">
 Current Trips
-</a>`
+</a> <br> <img src="MMpics/MM_gaut2.jpg" alt="Logo" style="width:120px;height:120px;border:0;"><img src="MMpics/MM_haiti4.jpg" alt="Logo" style="width:120px;height:120px;border:0;"><img src="MMpics/MM_london.jpg" alt="Logo" style="width:120px;height:120px;border:0;">`
 
     result();
 
